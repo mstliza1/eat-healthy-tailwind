ae19268d201aa97f5b3aedb7bceec5210c7951b4
mstliza1 <mstliza587@gamil.com>
2025-05-05 00:36:27 +0600

    added some important code

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundImage: {
+        
+          'shape-pattern': "url('/images/shapes/Group 8449.png')",
+        },
+      },
+    },
+    plugins: [],
+  }
